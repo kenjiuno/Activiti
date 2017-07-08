528906d87d05379a77e869cdaf7df097195225f2
kenjiuno <h8mastre@gmail.com>
2017-07-08 10:54:51 +0900

    no-cache on /model/{modelId}/json
    - Fix #2

--- modules/activiti-modeler/src/main/java/org/activiti/rest/editor/model/ModelEditorJsonRestResource.java
+++ modules/activiti-modeler/src/main/java/org/activiti/rest/editor/model/ModelEditorJsonRestResource.java
@@ -12,6 +12,11 @@
  */
 package org.activiti.rest.editor.model;
 
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
 import org.activiti.editor.constants.ModelDataJsonConstants;
 import org.activiti.engine.ActivitiException;
 import org.activiti.engine.RepositoryService;
@@ -20,6 +25,10 @@ import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -33,21 +42,21 @@ import com.fasterxml.jackson.databind.node.ObjectNode;
  */
 @RestController
 public class ModelEditorJsonRestResource implements ModelDataJsonConstants {
-  
+
   protected static final Logger LOGGER = LoggerFactory.getLogger(ModelEditorJsonRestResource.class);
-  
+
   @Autowired
   private RepositoryService repositoryService;
-  
+
   @Autowired
   private ObjectMapper objectMapper;
-  
-  @RequestMapping(value="/model/{modelId}/json", method = RequestMethod.GET, produces = "application/json")
-  public ObjectNode getEditorJson(@PathVariable String modelId) {
+
+  @RequestMapping(value = "/model/{modelId}/json", method = RequestMethod.GET, produces = "application/json")
+  public ResponseEntity<ObjectNode> getEditorJson(@PathVariable String modelId) {
     ObjectNode modelNode = null;
-    
+
     Model model = repositoryService.getModel(modelId);
-      
+
     if (model != null) {
       try {
         if (StringUtils.isNotEmpty(model.getMetaInfo())) {
@@ -57,15 +66,18 @@ public class ModelEditorJsonRestResource implements ModelDataJsonConstants {
           modelNode.put(MODEL_NAME, model.getName());
         }
         modelNode.put(MODEL_ID, model.getId());
-        ObjectNode editorJsonNode = (ObjectNode) objectMapper.readTree(
-            new String(repositoryService.getModelEditorSource(model.getId()), "utf-8"));
+        ObjectNode editorJsonNode = (ObjectNode) objectMapper
+            .readTree(new String(repositoryService.getModelEditorSource(model.getId()), "utf-8"));
         modelNode.put("model", editorJsonNode);
-        
+
       } catch (Exception e) {
         LOGGER.error("Error creating model JSON", e);
         throw new ActivitiException("Error creating model JSON", e);
       }
     }
-    return modelNode;
+
+    HttpHeaders headers = new HttpHeaders();
+    headers.setCacheControl("no-cache");
+    return new ResponseEntity<ObjectNode>(modelNode, headers, HttpStatus.OK);
   }
 }
